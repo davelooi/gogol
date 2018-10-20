62b316f484c177a9921578b5cc89721ca9ff2eed
David Looi <david@cultureamp.com>
2018-10-21 09:30:43 +1100

    Use test table instead of map

--- rules_test.go
+++ rules_test.go
@@ -15,38 +15,45 @@ func TestCellAlive(t *testing.T) {
 
 func TestDeadCellNextState(t *testing.T) {
   cell := Cell{Alive: false}
-  m := map[int]Cell{
-    0: {Alive: false},
-    1: {Alive: false},
-    2: {Alive: false},
-    3: {Alive: true},
-    4: {Alive: false},
-    5: {Alive: false},
-    6: {Alive: false},
-    7: {Alive: false},
-    8: {Alive: false},
+
+  tables := []struct {
+    neighbours int
+    expected   Cell
+  }{
+    {0, Cell{Alive: false}},
+    {1, Cell{Alive: false}},
+    {2, Cell{Alive: false}},
+    {3, Cell{Alive: true}},
+    {4, Cell{Alive: false}},
+    {5, Cell{Alive: false}},
+    {6, Cell{Alive: false}},
+    {7, Cell{Alive: false}},
+    {8, Cell{Alive: false}},
   }
 
-  for neighbours, expected := range m {
-    assert.Equal(t, expected, NextState(cell, neighbours))
+  for _, table := range tables {
+    assert.Equal(t, table.expected, NextState(cell, table.neighbours))
   }
 }
 
 func TestLivingCellNextState(t *testing.T) {
   cell := Cell{Alive: true}
-  m := map[int]Cell{
-    0: {Alive: false},
-    1: {Alive: false},
-    2: {Alive: true},
-    3: {Alive: true},
-    4: {Alive: false},
-    5: {Alive: false},
-    6: {Alive: false},
-    7: {Alive: false},
-    8: {Alive: false},
+  tables := []struct {
+    neighbours int
+    expected   Cell
+  }{
+    {0, Cell{Alive: false}},
+    {1, Cell{Alive: false}},
+    {2, Cell{Alive: true}},
+    {3, Cell{Alive: true}},
+    {4, Cell{Alive: false}},
+    {5, Cell{Alive: false}},
+    {6, Cell{Alive: false}},
+    {7, Cell{Alive: false}},
+    {8, Cell{Alive: false}},
   }
 
-  for neighbours, expected := range m {
-    assert.Equal(t, expected, NextState(cell, neighbours))
+  for _, table := range tables {
+    assert.Equal(t, table.expected, NextState(cell, table.neighbours))
   }
 }
