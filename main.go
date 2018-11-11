5bc635769f37cc56b44fb4cd26945e31b50c338e
David Looi <david@cultureamp.com>
2018-11-12 10:52:09 +1100

    Parse cli arguments

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
   "fmt"
   "math/rand"
+  "os"
+  "strconv"
 )
 
 func main() {
-  width := 60
-  height := 60
+  if len(os.Args) < 4 {
+    PrintHelp()
+    return
+  }
+
+  width := ParseUnlessError(os.Args[1])
+  height := ParseUnlessError(os.Args[2])
+  seed := ParseUnlessError(os.Args[3])
+
+  if width == -1 || height == -1 || seed == -1 {
+    return
+  }
 
   cells := make([][]Cell, 0)
   for h := 0; h < height; h++ {
     row := make([]Cell, 0)
     for w := 0; w < width; w++ {
-      row = append(row, Cell{x: w, y: h, Alive: RandomBool()})
+      row = append(row, Cell{x: w, y: h, Alive: RandomBool(seed)})
     }
     cells = append(cells, row)
   }
@@ -22,9 +34,26 @@ func main() {
   fmt.Print(Output(field))
 }
 
-func RandomBool() bool {
-  if rand.Intn(100) < 10 {
+func RandomBool(seed int) bool {
+  if rand.Intn(100) < seed {
     return true
   }
   return false
+}
+
+func ParseUnlessError(args string) int {
+  arg, err := strconv.Atoi(args)
+  if err != nil {
+    fmt.Printf("%s\n", err)
+    return -1
+  }
+  return arg
+}
+
+func PrintHelp() {
+  helpStatement := `
+  Usage:
+    ./gogol [width] [height] [seed]
+  `
+  fmt.Print(helpStatement)
 }
